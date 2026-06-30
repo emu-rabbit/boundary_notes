03d671f7e14502571d1f3535a5eb2b9e3294754f
絵夢羽さ沂 <mausu2526@gmail.com>
2026-06-30 09:51:27 +0800

    Fix: 調整前導劇情標記為低調文件夾

--- src/components/BrandMark.vue
+++ src/components/BrandMark.vue
@@ -9,8 +9,17 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <button class="brand-mark" type="button" @click="emit('restart')">
-    <span class="brand-orbit" aria-hidden="true" />
+  <button class="brand-mark" type="button" :aria-label="`${title}：重新開始前導劇情`" @click="emit('restart')">
+    <span class="brand-folder-shell" aria-hidden="true">
+      <svg class="brand-folder" viewBox="0 0 24 24" focusable="false">
+        <path
+          d="M3.75 6.75A2.25 2.25 0 0 1 6 4.5h4.2c.61 0 1.18.25 1.6.68l1.08 1.07H18A2.25 2.25 0 0 1 20.25 8.5v.75H3.75v-2.5Z"
+        />
+        <path
+          d="M3.75 8.25h16.5v8.5A2.25 2.25 0 0 1 18 19H6a2.25 2.25 0 0 1-2.25-2.25v-8.5Z"
+        />
+      </svg>
+    </span>
     <span>{{ title }}</span>
   </button>
 </template>
