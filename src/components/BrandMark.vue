d51ea46e1353f39b03c6dce61b8ee7a8d7581fa8
絵夢羽さ沂 <mausu2526@gmail.com>
2026-07-10 21:21:01 +0800

    Feat: 支援 BrandMark 靜態顯示模式

--- src/components/BrandMark.vue
+++ src/components/BrandMark.vue
@@ -4,6 +4,7 @@ import type { LocaleMessages } from '../app/i18n';
 defineProps<{
   hidden?: boolean;
   messages: LocaleMessages;
+  static?: boolean;
   title: string;
 }>();
 
@@ -13,7 +14,26 @@ const emit = defineEmits<{
 </script>
 
 <template>
+  <div
+    v-if="static"
+    class="brand-mark"
+    :class="{ 'is-visually-held': hidden }"
+  >
+    <span class="brand-folder-shell" aria-hidden="true">
+      <svg class="brand-folder" viewBox="0 0 24 24" focusable="false">
+        <path
+          d="M3.75 6.75A2.25 2.25 0 0 1 6 4.5h4.2c.61 0 1.18.25 1.6.68l1.08 1.07H18A2.25 2.25 0 0 1 20.25 8.5v.75H3.75v-2.5Z"
+        />
+        <path
+          d="M3.75 8.25h16.5v8.5A2.25 2.25 0 0 1 18 19H6a2.25 2.25 0 0 1-2.25-2.25v-8.5Z"
+        />
+      </svg>
+    </span>
+    <span>{{ title }}</span>
+  </div>
+
   <button
+    v-else
     class="brand-mark"
     :class="{ 'is-visually-held': hidden }"
     type="button"
